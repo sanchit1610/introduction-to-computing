c2a4d137203f687da1e8917e20967b78a0e17be5
sanchit1610 <97451818+sanchit1610@users.noreply.github.com>
2022-01-26 20:58:07 +0530

    Add files via upload

---
+++ assignment 2 answers.py
@@ -0,0 +1,54 @@
+Assignment-2 CSE
+SANCHIT THAKUR
+SID-21103126
+Answers
+
+
+Answers 1
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question 1.py ==
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+Answers 2
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question2.py ==
+ENTER YOUR NAME sanchit thakur
+ENTER YOUR SID 21103126
+ENTER YOUR DEPARTMENT computer science
+ENTER YOUR CGPA 10
+Hey  sanchit thakur, Here!
+My SID is 21103126
+I am from  computer science and my CGPA is 10.000000
+
+
+Answers 3
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question 3.py ==
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Answers 4
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question 4.py ==
+Enter first number: 10
+Enter second number: 15
+Enter third number: 7
+THE LARGEST NUMBER IS  15
+
+
+Answers 5
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question 5.py ==
+ENTER A STRING name
+Yes
+
+
+Answers 6
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 2\question 6.py ==
+ENTER FIRST SIDE OF TRIANGLE 2
+ENTER SECOND SIDE OF TRIANGLE 5
+ENTER THIRD SIDE OF TRIANGLE 6
+Yes
