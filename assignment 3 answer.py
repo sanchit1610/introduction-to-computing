2de56b85a96cff50cea2736ec87c9bc87401b329
sanchit1610 <97451818+sanchit1610@users.noreply.github.com>
2022-02-13 20:28:35 +0530

    Add files via upload

---
+++ assignment 3 answer.py
@@ -0,0 +1,86 @@
+Assignment-3 CSE
+SANCHIT THAKUR
+SID-21103126
+Answers
+
+
+Answer 1
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q1.py ======
+ENTER ANY STRING: sanchit
+{'s': 1, 'a': 1, 'n': 1, 'c': 1, 'h': 1, 'i': 1, 't': 1}
+
+Answer 2
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q2.py ======
+ENTER THE MONTH10
+ENTER THE DATE16
+ENTER THE YEAR2002
+Date- 16 / 10 / 2002
+Next Date- 17 / 10 / 2002
+
+
+Answer 3
+= RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\additional q3.py
+Enter elements of a list seperated by space 1 2 3 4 5 10 20 50 100
+list:  1 2 3 4 5 10 20 50 100
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (10, 100), (20, 400), (50, 2500), (100, 10000)]
+
+
+Answer 4
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q4.py ======
+Enter Grade Point: 9
+Your Grade is 'A' and Excellent performance
+
+
+Answer 5
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q5.py ======
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+
+Answer 6
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q6.py ======
+QUESTION 6
+Enter SID: 21103126
+Enter Name: SANCHIT THAKUR
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103130
+Enter Name: RAM
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103131
+Enter Name: SHAM
+Do you want to enter another student entry(Y or N):N
+<a> {21103126: 'SANCHIT THAKUR', 21103130: 'RAM', 21103131: 'SHAM'}
+<b> {21103130: 'RAM', 21103126: 'SANCHIT THAKUR', 21103131: 'SHAM'}
+<c> {21103126: 'SANCHIT THAKUR', 21103130: 'RAM', 21103131: 'SHAM'}
+Search Name with SID: 21103130
+<d> RAM
+
+
+Answer 7
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q7.py ======
+ENTER THE NUMBER OF TERMS IN THE SERIES: 10
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 8.8
+
+
+Answer 8
+====== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 3\q8.py ======
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
