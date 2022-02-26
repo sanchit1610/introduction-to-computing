d4d2c11e975f71dca51cef473aee1900110a0f41
sanchit1610 <97451818+sanchit1610@users.noreply.github.com>
2022-02-26 21:25:35 +0530

    Add files via upload

---
+++ assignment 4 answers.py
@@ -0,0 +1,82 @@
+Assignment 4 CSE
+sanchit thakur
+SID-21103126
+ANSWERS
+
+
+ANSWER 1
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\question1.py ==
+Move disk 1 source rod A to rod B
+Move disk 2 source rod A to rod C
+Move disk 1 source rod B to rod C
+Move disk 3 source rod A to rod B
+Move disk 1 source rod C to rod A
+Move disk 2 source rod C to rod B
+Move disk 1 source rod A to rod B
+Move disk 4 source rod A to rod C
+Move disk 1 source rod B to rod C
+Move disk 2 source rod B to rod A
+Move disk 1 source rod C to rod A
+Move disk 3 source rod B to rod C
+Move disk 1 source rod A to rod B
+Move disk 2 source rod A to rod C
+Move disk 1 source rod B to rod C
+
+
+ANSWER 2
+== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\questions2.py ==
+Enter the size of pascals triangle 6
+BY METHOD OF LOOPS
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+BY METHOD OF RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+
+ANSWER 3
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\question3.py ==
+Enter 2 numbers: 5 8
+A=. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<B>= The quotient is zero
+<C>= Filtered out numbers that are greater than 4 are : [9, 10, 5, 10, 6, 11]
+<D>= Set: {5, 6, 9, 10, 11}
+<E>= Immutable set: frozenset({5, 6, 9, 10, 11})
+<F>= Hash value of the max value from the above set: 11
+
+
+ANSWER 4
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\question4.py ==
+Object created
+The name of the student is sanchit and SID is 21103126.
+Object destroyed
+
+
+
+ANSWER 5
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\question5.py ==
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+
+
+ANSWER 6
+=== RESTART: C:\Users\THAKURS\Desktop\python assign\assignment 4\question6.py ==
+Enter the word: kite
+Enter the new meaningful word using the same exact letter to test the friendship between them: tike
+Does the new word makes sense?(y/Y or n/N )y
+The friends pass their friendship test
